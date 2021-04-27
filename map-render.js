e3d123be3ef9d73832692c429584ffef85e2d4e8
TobiaszWojnar <wojnar.tobiasz@gmail.com>
2021-04-27 21:50:22 +0200

    current state of my project

--- map-render.js
+++ map-render.js
@@ -23,19 +23,27 @@ let tooltip = d3.select("body").append("div")
     .style("opacity", 0);
 
 /*
+    Choosing color scheme, and scaling it for our data
+ */
+var colorScheme = d3.schemeYlOrRd[9];
+//colorScheme.unshift("#eee") //borrowed form example, a way to add color on the begining of array
+var colorScale = d3.scaleQuantile().domain([110,150])
+    .range(colorScheme);//https://observablehq.com/@d3/color-schemes
+
+/*
  3 functions for displaying data for regions, when mouse is over them.
  */
-let mouseover = function (d){
+let mouseover = ()=>{
     tooltip.transition()
         .duration(200)
         .style("opacity", .9);
 };
-let mousemove = function(d) {
+let mousemove = (d)=>{
     tooltip.html(d.properties.name +"<br>"+d.properties.rate)
-        .style("left", (d3.event.pageX+10) + "px")//.style("left", (d3.mouse(this)[0]+10) + "px")
+        .style("left", (d3.event.pageX+10) + "px")
         .style("top", (d3.event.pageY-5) + "px");
 };
-let mouseout = function(d) {
+let mouseout = ()=>{
     tooltip.transition()
         .duration(500)
         .style("opacity", 0);
@@ -49,7 +57,33 @@ Promise.all([jsonData]).then(function(data){
         .append("path")
         .attr("class","county")
         .attr("d", path)
+        .attr("fill",(e)=>{
+            return colorScale(e.properties.rate)
+        })
         .on("mouseover", mouseover)
         .on("mousemove",mousemove)
-        .on("mouseout", mouseout);
+        .on("mouseout", mouseout)
+        .on("click",(d)=>{
+            alert("County "+d.properties.name+"\r\n"+"Infection rate is "+d.properties.rate);}
+        );
+    /*
+    list of events can be found at https://www.w3schools.com/jsref/obj_mouseevent.asp
+     */
+
+    // Legend
+    var g = svg.append("g")
+        .attr("class", "legendThreshold")
+        .attr("transform", "translate(20,20)");
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", 0)
+        .attr("y", -6)
+        .text(data[0].name);//Or regular text
+    var labels = ['110', '115', '120', '125', '130', '135', '140','145','150'];
+    var legend = d3.legendColor()
+        .labels(function (d) { return labels[d.i]; })
+        .shapePadding(4)
+        .scale(colorScale);
+    svg.select(".legendThreshold")
+        .call(legend);
 });
